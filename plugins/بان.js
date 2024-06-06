3371f2a044b8e5fd4ca723833f23884b92238c3a
mvroo1 <jackalkber@gmail.com>
2024-06-06 10:41:41 +0300

    Update بان.js

--- plugins/بان.js
+++ plugins/بان.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!who) throw `□ منشن الشخص`
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `@${who.split`@`[0]} لن تستطيع استخدام اوامر بعد الان !`, m, { mentions: [who] })
+    conn.reply(m.chat, `@${who.split`@`[0]} مش هتستخدم البوت تاني 👽 !`, m, { mentions: [who] })
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
