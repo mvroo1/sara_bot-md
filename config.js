106d2d35ec426ac2ec9096306954b71ba76af75f
mvroo1 <jackalkber@gmail.com>
2024-06-06 10:12:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,21 +7,21 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "*『👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑』*"
+global.botnumber = "*『👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑』*"
 global.confirmCode = ""
 
-global.owner = [['201280244814', '𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩', true],
+global.owner = [['201208386317', '𝐽𝐴𝐶𝐾⁩', true],
 ];
 
-global.suittag = ['201280244814'];
-global.prems = ['201280244814'];
+global.suittag = ['201208386317'];
+global.prems = ['201208386317'];
 
-global.packname = '𝐻𝐴𝑅𝐿𝐸𝑌 𝐿𝐼𝐺𝐻𝑇⁩';
-global.author = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
-global.wm = '👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑';
-global.titulowm = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.titulowm2 = `☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`
-global.igfg = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
+global.packname = '𝐽𝐴𝐶𝐾';
+global.author = '👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑';
+global.wm = '👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑';
+global.titulowm = '☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎';
+global.titulowm2 = `☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎`
+global.igfg = '☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎';
 global.wait = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -42,15 +42,15 @@ global.mes = d.toLocaleDateString('ar', {month: 'long'});
 global.año = d.toLocaleDateString('ar', {year: 'numeric'});
 global.tiempo = d.toLocaleString('ar-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\n☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎`;
-global.gt = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.mysticbot = '☾︎ 👑𝑺𝐴𝑅𝐴-𝐵𝛩𝑇👑 ☽︎';
-global.md = 'https://chat.whatsapp.com/H93YpO0LkJNHoTmtIhcL1z';
+global.wm2 = `${dia} ${fecha}\n☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎`;
+global.gt = '☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎';
+global.mysticbot = '☾︎ 👑𝐽𝐴𝐶𝐾-𝐵𝛩𝑇👑 ☽︎';
+global.md = 'https://chat.whatsapp.com/B8eSVHOjh1Y9V8Kbjqbyez';
 global.mysticbot = 'atom.bio/harley_light';
 global.waitt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
 global.waittt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
 global.waitttt = '*[ ⏳ ] انتظر جاي التحميل تابع صاحب البوت*...\n\n*atom.bio/harley_light*';
-global.nomorown = '201280244814';
+global.nomorown = '201208386317';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
